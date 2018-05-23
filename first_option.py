03ab7f6e58e6e9fb67aa956a6e6f66585937b20f
Valera <NsT.valerka@ukr.net>
2018-05-23 03:24:41 +0300

    add algorithms

---
+++ first_option.py
@@ -0,0 +1,134 @@
+import random
+import sys
+
+
+def random_paired_servers(numbers_list, server_count):
+    """
+    Функция сортировки в случае парного кол-ва серверов
+    :param numbers_list: список реплик
+    :param server_count: кол-во серверов
+    :return: сервера с репликами
+    """
+    # Случайным образом заполняем первую половину серверов, а затем вторую и соединяем их
+    random.shuffle(numbers_list)
+    left_part = numbers_list
+    numbers_list = [i for i in range(1, server_count ** 2 + 1)]
+    random.shuffle(numbers_list)
+    right_part = numbers_list
+
+    return left_part + right_part
+
+
+def random_unpaired_servers(numbers_list, server_count, s_size):
+    """
+    Функция сортировки в случае непарного кол-ва серверов
+    :param numbers_list: список реплик
+    :param server_count: кол-во серверов
+    :param s_size: размер сервера
+    :return: сервера с репликами
+    """
+    array = []
+    random.shuffle(numbers_list)
+    array += numbers_list
+
+    # выделяем часть среднего сервера заполненого репликами первой части серверов
+    part = array[s_size * (server_count // 2): s_size * (server_count // 2) + s_size // 2]
+
+    # Дополняем средний сервер репликами из 2-й половины, которых нет в нем
+    counter = 0
+    for num in numbers_list:
+        if num not in part:  # проверяем, что бы в средний "общий сервер" не было записано дупликата
+            array.append(num)
+            part.append(num)
+            numbers_list.pop(numbers_list.index(num))
+            counter += 1
+        if counter == s_size: # проверяем есть ли место на сервере для реплик
+            break
+    array += numbers_list
+
+    return array
+
+
+def generate_servers(server_count):
+    """
+    Функция генерации серверов с репликами
+    :param server_count: кол-во северов
+    :return: сервара с репликами
+    """
+    s_size = 2 * server_count
+    numbers_list = [i for i in range(1, server_count ** 2 + 1)]
+
+    if server_count % 2 == 0:
+        array = random_paired_servers(numbers_list, server_count)
+    else:
+        array = random_unpaired_servers(numbers_list, server_count, s_size)
+
+    # сортируем реплики
+    servers = []
+    for i in range(server_count):
+        servers.append(array[i * s_size: i * s_size + s_size])
+        servers[i].sort()
+    servers.sort()
+
+    return servers
+
+
+def generate_mirror(server_count):
+    """
+    Функция генерации симметричных серверов
+    :param server_count: кол-во северов
+    :return: сервара с репликами
+    """
+    array = []
+    s_size = 2 * server_count
+    numbers_list = [i for i in range(1, server_count ** 2 + 1)]
+    if server_count % 2 == 0:
+        random.shuffle(numbers_list)
+        array = numbers_list + numbers_list
+
+    servers = []
+    for i in range(server_count):
+        servers.append(array[i * s_size: i * s_size + s_size])
+        servers[i].sort()
+    servers.sort()
+
+    return servers
+
+
+def calculate_probability_failure(servers):
+    """
+    :param servers: список серверов
+    :return: словарь: ключ - № сервера, значение - вероятность потери данных
+    """
+    server_count = len(servers)  # количество серверов
+    loose_chance = 100 / (server_count - 1)  # шанс потери данных, если сервер падает
+    chances = {}
+    for i in range(server_count):
+        chances[i] = 0
+
+    # проходим с первого до предпоследнего (включительно) сервера сравниваем его реплики
+    # c последующими за ним серверами, если есть совпадения, то добавляем вероятность потери
+    for first in range(server_count - 1):
+        for second in range(first + 1, server_count):
+            for i in servers[first]:
+                if i in servers[second]:
+                    chances[first] += loose_chance
+                    chances[second] += loose_chance
+                    break
+
+    return chances
+
+
+if __name__ == "__main__":
+    if sys.argv[3] in ["--random", "--mirror"]:
+        if sys.argv[3] == "--random":
+            servers = generate_servers(int(sys.argv[2]))
+        elif sys.argv[3] == "--mirror":
+            if int(sys.argv[2]) % 2 != 0:
+                print("Wrong n for mirror! n can be k * 2 (2, 4, 6 ...)")
+                exit()
+            servers = generate_mirror(int(sys.argv[2]))
+
+        result = calculate_probability_failure(servers)
+        for key in result:
+            print("Killing {} arbitrary servers results in data loss in {} cases".format(key + 1, result[key]))
---
+++ second_option.py
@@ -0,0 +1,76 @@
+from first_option import generate_servers, generate_mirror
+from copy import deepcopy
+import sys
+
+
+def translate_servers(servers):
+    """
+    Переводим список в последовтельность битов, где значения списка это позиции битов
+    имеющих значени 1, например, [1, 2, 4] -> 0b1011
+    :param servers: список серверов [[...], [...], ...]
+    :return: список серверов представленных последовательностью бит
+    """
+    array = []
+
+    for index, server in enumerate(servers):
+        bits_array = 0b0
+        for replica in server:
+            bits_array |= 0b1 << (replica - 1)
+
+        array.append(bits_array)
+
+    return array
+
+
+def calculate_probability_failure(servers):
+    """
+    :param servers: список серверов представленных последовательностью бит
+    :param n: кол-во серверов
+    :return: словарь: ключ - № сервера, значение - вероятность потери данных
+    """
+    # Словарь: хранящий ключ - № сервера,
+    # значение - вероятность потери данных при падении сервера
+    server_count = len(servers)
+    chances = {}
+    for i in range(server_count):
+        chances[i] = 0
+    loose_chance = 100 / (server_count - 1)  # шанс потери данных, если сервер падает (в нашем случае все равны)
+
+    data = deepcopy(servers)  # делаем копию входных данных, что бы не изменить входные данные в процессе обработки
+
+    for index in range(server_count - 1):
+        next_server = index
+
+        while data[index] != 0:
+            next_server += 1
+
+            # Операцией "И" получаем битовую последовательность, где 1 стоят на позициях
+            # в которых и в первой и в второй последовательностях стоят 1.
+            result = data[index] & data[next_server]
+            if result != 0:
+                # наличие единичных битов в одинаковых позициях означает, что сервера
+                # имеют одинаковые реплики данных. Поэтому добавляем вероятность потери.
+                chances[index] += loose_chance
+                chances[next_server] += loose_chance
+                # обнуляем совпавшие биты
+                data[index] = data[index] & (~ result)
+                data[next_server] = data[next_server] & (~ result)
+
+    return chances
+
+
+if __name__ == "__main__":
+    if sys.argv[3] in ["--random", "--mirror"]:
+        if sys.argv[3] == "--random":
+            servers = generate_servers(int(sys.argv[2]))
+            servers = translate_servers(servers)
+        elif sys.argv[3] == "--mirror":
+            if int(sys.argv[2]) % 2 != 0:
+                print("Wrong n for mirror! n can be k * 2 (2, 4, 6 ...)")
+                exit()
+            servers = generate_mirror(int(sys.argv[2]))
+            servers = translate_servers(servers)
+
+        result = calculate_probability_failure(servers)
+        for key in result:
+            print("Killing {} arbitrary servers results in data loss in {} cases".format(key + 1, result[key]))
